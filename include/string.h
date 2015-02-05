5539aa460c67c105da29c413f567fda4277cab57
Scott Harvey <scott.harvey@stonybrook.edu>
2015-02-04 19:22:13 -0500

    Add Linux COPYING, string.c, string.h
    As long as the COPYING file is included modifying the Linux source does
    not conflict with any of the licenses.

---
+++ include/string.h
@@ -0,0 +1,33 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+#define NULL ((void*)0)
+
+int      memcmp(const void *, const void *, size_t);
+void    *memcpy(void *restrict, const void *restrict, size_t);
+int      memcmp(const void *, const void *, size_t);
+void    *memcpy(void *restrict, const void *restrict, size_t);
+char    *strcat(char *restrict, const char *restrict);
+char    *strchr(const char *, int);
+int      strcmp(const char *, const char *);
+int      strcoll(const char *, const char *);
+char    *strcpy(char *restrict, const char *restrict);
+size_t   strcspn(const char *, const char *);
+
+char    *strdup(const char *);
+
+char    *strerror(int);
+
+int     *strerror_r(int, char *, size_t);
+
+size_t   strlen(const char *);
+char    *strncat(char *restrict, const char *restrict, size_t);
+int      strncmp(const char *, const char *, size_t);
+char    *strncpy(char *restrict, const char *restrict, size_t);
+char    *strpbrk(const char *, const char *);
+char    *strrchr(const char *, int);
+size_t   strspn(const char *, const char *);
+char    *strstr(const char *, const char *);
+char    *strtok(char *restrict, const char *restrict);
+
+#endif /* _STRING_H */
