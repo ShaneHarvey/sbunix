556f87979312b68024e4fdf4b79d53cb9f55a747
Shane Harvey <shane6harvey@yahoo.com>
2015-02-07 18:32:00 -0500

    sblibc
    Added printf with integer and string format support.
    Added ctype header with standard ascii macros
    Added test file that prints argc, argv, and envp

--- include/ctype.h
+++ include/ctype.h
@@ -1,70 +1,42 @@
-#ifndef _LINUX_CTYPE_H
-#define _LINUX_CTYPE_H
+#ifndef	_CTYPE_H
+#define	_CTYPE_H	1
 
-/*
- * NOTE! This ctype does not handle EOF like the standard C
- * library is required to.
- */
+//__BEGIN_DECLS
+// checks for an alphanumeric character
+#define isalnum(c)  (isalpha(c) || isdigit(c))
+// checks for an alphabetic character
+#define isalpha(c)  (isupper(c) || islower(c))
+// checks whether c is a 7-bit unsigned char value that fits into the ASCII
+// character set
+#define isascii(c)  (!((c) & ~0x7f))
+// isblank() checks for a blank character; that is, a space or a tab
+#define isblank(c)  ((c) == ' ' || (c) == '\t')
+// iscntrl() checks for a control character.
+#define iscntrl(c)  ()
+// isdigit() checks for a digit (0 through 9).
+#define isdigit(c)  ((c) >= '0' && (c) <= '9')
+// isgraph() checks for any printable character except space.
+#define isgraph(c)  (isascii(c) && (c) > ' ')
+// islower() checks for a lowercase character.
+#define islower(c)  ((c) >= 'a' && (c) <= 'z')
+// isprint() checks for any printable character including space.
+#define isprint(c)  (isascii(c) && (c) >= ' ')
+// ispunct() checks for any printable character which is not a space or an
+// alphanumeric character.
+#define ispunct(c)  (isgraph(c) && !isalnum(c))
+// isspace() checks for white-space characters. In the "C" and "POSIX" locales,
+// these are: space, form-feed ('\f'), newline ('\n'), carriage return ('\r'),
+// horizontal tab ('\t'), and vertical tab ('\v').
+#define isspace(c)  ((c) == ' ' || (c) == '\f' || (c) == '\n' || (c) == '\r' || (c) == '\t' || (c) == '\v')
+// isupper() checks for an uppercase letter.
+#define isupper(c)  ((c) >= 'A' && (c) <= 'Z')
+// isxdigit() checks for hexadecimal digits, that is,
+// one of 0 1 2 3 4 5 6 7 8 9 a b c d e f A B C D E F.
+#define isxdigit(c) (isdigit(c) || (c) >= 'a' && (c) <= 'f' || (c) >= 'A' && (c) <= 'Z')
 
-#define _U	0x01	/* upper */
-#define _L	0x02	/* lower */
-#define _D	0x04	/* digit */
-#define _C	0x08	/* cntrl */
-#define _P	0x10	/* punct */
-#define _S	0x20	/* white space (space/lf/tab) */
-#define _X	0x40	/* hex digit */
-#define _SP	0x80	/* hard space (0x20) */
 
-extern const unsigned char _ctype[];
-
-#define __ismask(x) (_ctype[(int)(unsigned char)(x)])
-
-#define isalnum(c)	((__ismask(c)&(_U|_L|_D)) != 0)
-#define isalpha(c)	((__ismask(c)&(_U|_L)) != 0)
-#define iscntrl(c)	((__ismask(c)&(_C)) != 0)
-#define isdigit(c)	((__ismask(c)&(_D)) != 0)
-#define isgraph(c)	((__ismask(c)&(_P|_U|_L|_D)) != 0)
-#define islower(c)	((__ismask(c)&(_L)) != 0)
-#define isprint(c)	((__ismask(c)&(_P|_U|_L|_D|_SP)) != 0)
-#define ispunct(c)	((__ismask(c)&(_P)) != 0)
-/* Note: isspace() must return false for %NUL-terminator */
-#define isspace(c)	((__ismask(c)&(_S)) != 0)
-#define isupper(c)	((__ismask(c)&(_U)) != 0)
-#define isxdigit(c)	((__ismask(c)&(_D|_X)) != 0)
-
-#define isascii(c) (((unsigned char)(c))<=0x7f)
-#define toascii(c) (((unsigned char)(c))&0x7f)
-
-static inline unsigned char __tolower(unsigned char c)
-{
-	if (isupper(c))
-		c -= 'A'-'a';
-	return c;
-}
-
-static inline unsigned char __toupper(unsigned char c)
-{
-	if (islower(c))
-		c -= 'a'-'A';
-	return c;
-}
-
-#define tolower(c) __tolower(c)
-#define toupper(c) __toupper(c)
-
-/*
- * Fast implementation of tolower() for internal usage. Do not use in your
- * code.
- */
-static inline char _tolower(const char c)
-{
-	return c | 0x20;
-}
-
-/* Fast check for octal digit */
-static inline int isodigit(const char c)
-{
-	return c >= '0' && c <= '7';
-}
+#define tolower(c)  (isupper(c)? (c) - ('a' - 'A') : (c))
+#define toupper(c)  (islower(c)? (c) + ('a' - 'A') : (c))
+//__END_DECLS
 
 #endif
