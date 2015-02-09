754436dcc349c574f4bc2849bd1bf8b7af9f5e03
Shane Harvey <shane6harvey@yahoo.com>
2015-02-08 23:51:28 -0500

    Added getenv(3)

--- libc/getenv.c
+++ libc/getenv.c
@@ -3,10 +3,19 @@
 #include <string.h>
 
 char *getenv(const char *name) {
-    int i;
-
+    int i, len;
+    if(name == NULL || *name == '\0') {
+        return NULL;
+    }
+    len = strlen(name);
+    /* search for name */
     for(i = 0; __environ[i] != NULL; i++) {
-        /* search for name */
+        char *eptr = strchr(__environ[i], '=');
+
+        /* length and bytes are equal */
+        if(len == eptr - __environ[i] && !strncmp(name, __environ[i], len)) {
+            return eptr + 1;
+        }
     }
     return NULL;
 }
