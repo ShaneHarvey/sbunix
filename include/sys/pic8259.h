aa463f1a5ff912d9748d557dda92e26b878c9bda
Shane Harvey <shane6harvey@yahoo.com>
2015-03-01 18:53:22 -0500

    Added routines for PIC remapping and IRQ masking

--- include/sys/pic8259.h
+++ include/sys/pic8259.h
@@ -1,12 +1,28 @@
 #ifndef _PIC8259_H
 #define _PIC8259_H
 
+#include <sys/defs.h>
+
 /* Master 8259 PIC Ports */
-#define PIC_M_CMD   0x020
-#define PIC_M_DATA  0x021
+#define PIC_M_CMD   0x20
+#define PIC_M_DATA  0x21
 
 /* Slave 8259 PIC Ports */
-#define PIC_S_CMD   0x0A0
-#define PIC_S_DATA  0x0A1
+#define PIC_S_CMD   0xA0
+#define PIC_S_DATA  0xA1
+
+/* PIC Setup */
+void PIC_protcted_mode(void);
+void PIC_remap(uint8_t offset1, uint8_t offset2);
+
+/* Interrupt Routines */
+void PIC_sendEOI(uint8_t irq);
+
+void IRQ_set_mask(uint8_t IRQline);
+void IRQ_clear_mask(uint8_t IRQline);
+
+uint16_t pic_get_irr(void);
+uint16_t pic_get_isr(void);
+
 
 #endif
