e0870feb5fa5ab0407c7da31063c4be7c5e98110
Shane Harvey <shane6harvey@yahoo.com>
2015-05-03 00:51:15 -0400

    Change user stack to be highest lower half canonical address

--- include/sbunix/mm/vmm.h
+++ include/sbunix/mm/vmm.h
@@ -4,9 +4,9 @@
 #include <sbunix/sbunix.h>
 #include <sbunix/mm/types.h>
 
-/* Start stack for users, maps to top of pml4[479] */
-#define USER_STACK_START 0xffffeffffffff000ULL
-#define USER_STACK_END   (USER_STACK_START - (20 * PAGE_SIZE))
+/* Start stack for users, maps to pml4[255]->pdpt[511]->pd[511]->pt[511] */
+#define USER_STACK_START 0x00007fffffffffffULL
+#define USER_STACK_END   (USER_STACK_START - (20ULL * PAGE_SIZE))
 
 /* mm_struct functions */
 
