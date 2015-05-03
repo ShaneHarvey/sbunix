2b8d2fbfd4573444f3bafcdc4fd5bae458f4a5f6
Shane Harvey <shane6harvey@yahoo.com>
2015-05-03 15:45:05 -0400

    Set errno  explicity in hello

--- bin/hello/hello.c
+++ bin/hello/hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char* argv[], char* envp[]) {
-	/*printf("Hello World!\n");*/
+    /*printf("Hello World!\n");*/
+    errno = 10;
     exit(122);
-	return 244;
+    return 244;
 }
