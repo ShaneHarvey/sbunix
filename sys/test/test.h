017618819d2281ef9fad21ffac0e4621541f9b98
Shane Harvey <shane6harvey@yahoo.com>
2015-05-05 10:51:58 -0400

    Move terminal test to test dir

--- sys/test/test.h
+++ sys/test/test.h
@@ -4,10 +4,12 @@
 #include <sbunix/sbunix.h>
 #include <sbunix/syscall.h>
 #include <sbunix/fs/elf64.h>
+#include <sbunix/fs/terminal.h>
 #include <sbunix/sched.h>
 #include <sbunix/string.h>
 
-
 void test_exec(void);
+void test_terminal(void);
+void test_pipe(void);
 
 #endif //_SBUNIX_TEST_H
