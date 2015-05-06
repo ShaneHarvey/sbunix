49aefb20dbde93f554b044c36fd82e058262e136
Shane Harvey <shane6harvey@yahoo.com>
2015-05-06 00:56:54 -0400

    User can write to terminal! WOOT

--- sys/kmain.c
+++ sys/kmain.c
@@ -1,11 +1,7 @@
 #include "kmain.h"
 #include "sched/roundrobin.h"
 #include <sbunix/sbunix.h>
-#include <sbunix/sched.h>
-#include <sbunix/syscall.h>
-#include <sbunix/string.h>
 #include <sbunix/console.h>
-#include <sbunix/fs/terminal.h>
 
 #include "test/test.h"
 
@@ -17,12 +13,13 @@ void kmain(void) {
     clear_console();
     printk("*** Welcome to SBUnix ***\n");
 
-    task_files_init(&kernel_task);
     ktask_create(test_exec, "TestExec");
 
 
+    /* idle task */
     while(1){
         schedule();
+        __asm__ __volatile__("sti;hlt;");
     }
 
     kpanic("\nReturned to kmain!!!\n");
