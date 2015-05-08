95be7ee75fe3cdbbb97b8e152b472e0190f6825b
Shane Harvey <shane6harvey@yahoo.com>
2015-05-08 11:59:26 -0400

    Fix ls on "/" and fix bug in pipe_open

--- sys/fs/tarfs.c
+++ sys/fs/tarfs.c
@@ -306,9 +306,11 @@ int tarfs_readdir(struct file *filep, void *buf, unsigned int count) {
     size_t len, elen;
     const char *entryname;
     unsigned char type;
+    int is_root;
 
 
     fhdr = filep->private_data;
+    is_root = &fs_root_hdr == fhdr;
     len = strlen(fhdr->name);
 //    printk("READDIR: %s\n", fhdr->name);
 
@@ -316,11 +318,11 @@ int tarfs_readdir(struct file *filep, void *buf, unsigned int count) {
     for( ;next != NULL; next = tarfs_next(next)) {
 //        printk("READDIR: next header: %s\n", next->name);
         /* check in next's name begins with this dir name */
-        if(strncmp(fhdr->name, next->name, len) != 0)
+        if(!is_root && strncmp(fhdr->name, next->name, len) != 0)
             break; /* no more entries */
 
         /* filename on potential entry */
-        entryname = next->name + len + 1;
+        entryname = next->name + len + 1 - is_root;
         if(strchr(entryname, '/'))
             continue; /* skip to next header */
 
