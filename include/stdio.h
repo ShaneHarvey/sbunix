02b2c1f2559267fd56c11c932a7a54eea50eff0d
Shane Harvey <shane6harvey@yahoo.com>
2015-05-09 16:29:40 -0400

    snprintf

--- include/stdio.h
+++ include/stdio.h
@@ -1,7 +1,10 @@
 #ifndef _STDIO_H
 #define _STDIO_H
 
+#include <sys/types.h>
+
 int printf(const char *fmt, ...) __attribute__((format(printf,1,2)));
 int scanf(const char *format, ...) __attribute__((format(scanf,1,2)));
+int snprintf(char *str, size_t size, const char *fmt, ...) __attribute__((format(printf,3,4)));
 
 #endif
